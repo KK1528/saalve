a9ab471467274aaac5a509232d1740f2c1616423
kk1528 <2020UEC1528@mnit.ac.in>
2024-03-30 18:44:09 +0530

    added the logout functionality

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA8HzZWOv8jRsnPHoXB49oIZF1jDkrSwJc",
-  authDomain: "saalve-a460d.firebaseapp.com",
-  projectId: "saalve-a460d",
-  storageBucket: "saalve-a460d.appspot.com",
-  messagingSenderId: "729058644368",
-  appId: "1:729058644368:web:736e1b46226f08c8722315",
-  measurementId: "G-747D8V4V0M"
+  apiKey: "AIzaSyBahsKNz8yth-rvRiNWB99XRxzxHPvqE9I",
+  authDomain: "saalve-a94ed.firebaseapp.com",
+  projectId: "saalve-a94ed",
+  storageBucket: "saalve-a94ed.appspot.com",
+  messagingSenderId: "286991439140",
+  appId: "1:286991439140:web:f486348e137b74a03b4fb1",
+  measurementId: "G-LFR8CNZZ52"
 };
 
 const app = initializeApp(firebaseConfig);
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,54 +1,61 @@
-import React,{useState} from 'react';
-import {Link} from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 import './navbar.css';
 
 
 function NavBar(props) {
-  const [active,setActive] = useState({meal: false, grocery: false, bakery: false, partner: false});
-  const [toggle,setToggle] = useState(true);
-  const [notifications,setNotifications] = useState(false);
-  
-  
-  
-    return (
-      <nav className="nav">
-        <ul className="nav-list">
-            <li><a href="/" className="nav-logo">SAALVE</a></li>
-            <li className="nav-toggle">
-              <button  
-                onClick={() => setToggle(!toggle)}>
-                <span className="material-icons">{toggle?"menu":"close"}</span>
-                  </button></li>
-            <div className={toggle? "nav-menu visible":"nav-menu" }>
-              <li className="buttons">
-                <Link to='/meal' 
-                  onClick={() => {setActive(prev => {return {meal: true, grocery: false, bakery: false, partner: false}});setToggle(!toggle)}}
-                  className={active.meal? "active": "" }>Meal</Link>
-              </li>
-              <li className="buttons">
-                <Link to='/grocery'
-                  onClick={() => {setActive(prev => {return {meal: false, grocery: true, bakery: false, partner: false}});setToggle(!toggle)}} 
-                  className={active.grocery? "active": "" }>Grocery</Link>
-              </li>
-              {/* <li className="buttons">
+  const [active, setActive] = useState({ meal: false, grocery: false, bakery: false, partner: false });
+  const [toggle, setToggle] = useState(true);
+  // const [notifications, setNotifications] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Perform logout actions here
+    localStorage.removeItem('Isloggedin-id');
+    navigate('/');
+    window.location.reload();
+  };
+
+  return (
+    <nav className="nav">
+      <ul className="nav-list">
+        <li><a href="/" className="nav-logo">SAALVE</a></li>
+        <li className="nav-toggle">
+          <button
+            onClick={() => setToggle(!toggle)}>
+            <span className="material-icons">{toggle ? "menu" : "close"}</span>
+          </button></li>
+        <div className={toggle ? "nav-menu visible" : "nav-menu"}>
+          <li className="buttons">
+            <Link to='/meal'
+              onClick={() => { setActive(prev => { return { meal: true, grocery: false, bakery: false, partner: false } }); setToggle(!toggle) }}
+              className={active.meal ? "active" : ""}>Meal</Link>
+          </li>
+          <li className="buttons">
+            <Link to='/grocery'
+              onClick={() => { setActive(prev => { return { meal: false, grocery: true, bakery: false, partner: false } }); setToggle(!toggle) }}
+              className={active.grocery ? "active" : ""}>Grocery</Link>
+          </li>
+          {/* <li className="buttons">
                 <Link to='/bakery'
                   onClick={() => {setActive(prev => {return {meal: false, grocery: false, bakery: true, partner: false}});setToggle(!toggle)}} 
                   className={active.bakery? "active": "" }>Bakery</Link>
               </li> */}
-              <li className="buttons">
-                <Link to='/contactus'
-                  onClick={() => {setActive(prev => {return {meal: false, grocery: false, bakery: false, partner: true}});setToggle(!toggle)}} 
-                  className={active.partner? "active": "" }>Contact us</Link>
-              </li>
-              <li className="buttons">
-                <Link to='/account' onClick={() => {setActive(prev => {return {meal: false, grocery: false, bakery: false, partner: false}});setToggle(!toggle)}}>Account</Link>
-              </li>
-            </div>
-            <div className="nav-icons">
-              {/* <li className="nav-icon">
+          <li className="buttons">
+            <Link to='/contactus'
+              onClick={() => { setActive(prev => { return { meal: false, grocery: false, bakery: false, partner: true } }); setToggle(!toggle) }}
+              className={active.partner ? "active" : ""}>Contact us</Link>
+          </li>
+          <li className="buttons">
+            <Link to='/account' onClick={() => { setActive(prev => { return { meal: false, grocery: false, bakery: false, partner: false } }); setToggle(!toggle) }}>Account</Link>
+          </li>
+        </div>
+        <div className="nav-icons">
+          {/* <li className="nav-icon">
                 <Link to='/favorite' onClick={() => {setActive(prev => {return {meal: false, grocery: false, bakery: false, partner: false}})}}><span className="material-icons">favorite</span></Link>
               </li> */}
-              {/* <li className="nav-icon">
+          {/* <li className="nav-icon">
                 <span className="material-icons" onClick={() => setNotifications(!notifications)}>notifications</span>
                 <div className='counter'>2</div>
                 <div className={notifications?"nav-notifications":"invisible"}>
@@ -60,23 +67,37 @@ function NavBar(props) {
                   </ul>
                 </div>
               </li> */}
-              
-                {(!props.loginVar)?
-                <div className='signin' style={{marginTop:"1.1rem"}}>
-                  <Link to="/signinpage">Sign-up</Link>
-                  <Link to="/login">Log-in</Link>
-                </div>:
-               <> <li className="nav-icon">
-                <Link to='/cart' onClick={() => {setActive(prev => {return {meal: false, grocery: false, bakery: false, partner: false}})}}><span className="material-icons">shopping_cart</span></Link>
+
+          {(props.loginVar) ?
+            <>
+              <li className="nav-icon">
+                <Link to='/cart' onClick={() => { setActive(prev => { return { meal: false, grocery: false, bakery: false, partner: false } }) }}>
+                  <span className="material-icons">shopping_cart</span>
+                </Link>
                 <div className='counter'>{props.cartSize}</div>
               </li>
               <li className="nav-icon invi_small">
-                <Link to='/account' onClick={() => {setActive(prev => {return {meal: false, grocery: false, bakery: false, partner: false}})}}><span className="material-icons">face</span></Link>
-              </li></>}
+                <Link to='/account' onClick={() => { setActive(prev => { return { meal: false, grocery: false, bakery: false, partner: false } }) }}>
+                  <span className="material-icons">face</span>
+                </Link>
+                <button style={{
+                  backgroundColor: 'transparent',
+                  border: 'none',
+                  cursor: 'pointer',
+                  outline: 'none',
+                  padding: '0',
+                }} onClick={handleLogout}><span className="material-icons">logout</span></button>
+              </li>
+            </> :
+            <div className='signin' style={{ marginTop: "1.1rem" }}>
+              <Link to="/signinpage">Sign-up</Link>
+              <Link to="/login">Log-in</Link>
             </div>
-        </ul>
-      </nav>
-    );
-  }
-  
-  export default NavBar;
+          }
+        </div>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
